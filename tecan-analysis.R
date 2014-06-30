63040576d0a81f1c4bf25b8bdd05084cd616036c
Nicholas McGlincy <nmcglincy@gmail.com>
2014-06-30 11:07:46 -0700

    Examining relationship between temp & cycle.no

--- tecan-analysis.R
+++ tecan-analysis.R
@@ -7,43 +7,85 @@ header = read.csv("20140627-gcn20arb1del.csv",
 no.cycles = as.integer(header[grep("Kinetic Cycles", header$V1),5])
 no.cycles
 
+
 tecanData = read.csv("20140627-gcn20arb1del.csv",
 						skip = 35,
 						header = TRUE,
 						check.names = FALSE)
+
 # Some sort of class problem with the first two columns
-str(tecanData)
-class(tecanData)
-head(tecanData[,1:5])
-#?colnames
 col.names = colnames(tecanData)
 col.names
 col.names[1] = "cycle.no"
 col.names[2] = "temp.C"
 colnames(tecanData) = col.names
+head(tecanData[,1:10])
 #?tail
 tail(tecanData)
 dim(tecanData)
-tecanData = tecanData[1:96,]
+tecanData = tecanData[1:no.cycles,]
 # Would be cool if I could extract the cycle name from the tecan 
 # results file.
 #
 dim(tecanData)
 tail(tecanData)
 #
-# Separate off the temperature data
-head(tecanData)
-temp.data = tecanData[,1:2]
-temp.data
-#
+# # Separate off the temperature data
+# head(tecanData)
+# temp.data = tecanData[,1:2]
+# temp.data
+# #
+# length(tecanData)
+# tecanData = tecanData[,3:length(tecanData)]
+# head(tecanData)
+# #
+# Plot the temperature data
+# head(temp.data)
+# str(temp.data)
+with(tecanData, plot(cycle.no, temp.C))
+# yes, it's reading the first two columns as factors somehow...
+tecanData$cycle.no = as.character(tecanData$cycle.no)
+tecanData$cycle.no = as.integer(tecanData$cycle.no)
+# 
+# As a one-liner
+tecanData$cycle.no = as.integer(as.character(tecanData$cycle.no))
+tecanData$temp.C = as.numeric(as.character(tecanData$temp.C))
+# str(tecanData)
+# Plotting temperature as a function of cycle no.; examine the evidence for an association
+with(tecanData, plot(cycle.no, temp.C))
+cor(tecanData$cycle.no , tecanData$temp.C, method = "spearman")
+cor(tecanData$cycle.no , tecanData$temp.C, method = "pearson")
+with(tecanData, abline(lm(temp.C ~ cycle.no)))
+# black line on the plot
+# 
+with(tecanData, summary(lm(temp.C ~ cycle.no)))
+# 
+# Call:
+#     lm(formula = temp.C ~ cycle.no)
+# 
+# Residuals:
+#     Min       1Q   Median       3Q      Max 
+# -0.62489 -0.20911  0.01839  0.22285  0.48854 
+# 
+# Coefficients:
+#                   Estimate    Std. Error t value  Pr(>|t|)    
+#       (Intercept) 30.163575   0.058421    516.312 <2e-16 ***
+#       cycle.no     0.001803   0.001046    1.724   0.0879 .  
+# ---
+#     Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+# 
+# Residual standard error: 0.284 on 94 degrees of freedom
+# Multiple R-squared:  0.03066,    Adjusted R-squared:  0.02035 
+# F-statistic: 2.973 on 1 and 94 DF,  p-value: 0.08793
+# 
+# Mild increase in temperature with cycle.no, but narrowly misses significance
+# 
+with(tecanData, abline(line(cycle.no, temp.C), col = "red"))
+# Robustly fitting line, following the method of Tukey in EDA. Coloured red. 
+# Relationship looks a bit more deterministic, but still within a very narrow range.
+# 
+# Remove the temperature data from the dataframe
+head(tecanData[,1:10])
 length(tecanData)
 tecanData = tecanData[,3:length(tecanData)]
-head(tecanData)
-#
-# Plot the temperature data
-head(temp.data)
-str(temp.data)
-temp.data$cycle.no = numeric(temp.data$cycle.no)
-temp.data$temp.C = numeric(temp.data$temp.C)
-str(temp.data)
-with(temp.data, plot(cycle.no, temp.C, type = "line"))
+# 
