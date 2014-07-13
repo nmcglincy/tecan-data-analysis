205816d8854dc692217ad3b925ad762d50f32143
Nicholas McGlincy <nmcglincy@gmail.com>
2014-07-12 17:47:12 -0700

    Updating from Saturday work

--- establishing-the-proper-time-base.R
+++ establishing-the-proper-time-base.R
@@ -1,28 +1,44 @@
 # Constructing a proper time base for tecan expt
-timeData = read.csv("20140627-gcn20arb1del-justTheTime.csv", header = TRUE, stringsAsFactors = FALSE)
-head(timeData)
+header = read.csv("20140627-gcn20arb1del.csv",
+                header = FALSE,
+                nrows = 35,
+                colClasses = "character")
+no.cycles = as.integer(header[grep("Kinetic Cycles", header$V1),5])
+timeData = read.csv("20140627-gcn20arb1del.csv", 
+					header = TRUE, 
+					stringsAsFactors = FALSE,
+					skip = 35,
+					check.names = FALSE)
+col.names = colnames(timeData)
+col.names[1] = "cycle.no"
+col.names[2] = "temp.C"
+colnames(timeData) = col.names
+timeData = timeData[1:no.cycles, 3:length(timeData)]
+# 
+# Now I've got just the data; I want to extract the time values
 # 
 # removing the absorbance data
-dim(timeData)
-length(timeData)
-odd.columns = seq(from = 1, to = length(timeData), by = 2)
-odd.columns
-timeData = timeData[,odd.columns]
-head(timeData)
+even.columns = seq(from = 2, to = length(timeData), by = 2)
+# even.columns
+timeData = timeData[,even.columns]
+# head(timeData)
+# 
+# Specificying the right column names
+plate.letters = rep(LETTERS[1:8], each = 12)
+plate.numbers = rep(1:12, 8)
+wells = paste(plate.letters, plate.numbers, sep = "")
+# wells
+colnames(timeData) = wells
+# head(timeData)
 # 
 # calculating the average cycle to cycle interval, where the recorded data is in a sensible format
-head(timeData, 20)
+# head(timeData, 20)
 # use the first 11 cycles/rows
 # 
-?diff
-(1:10)^2
-diff(x = (1:10)^2)
-# 
 good.timeData = timeData[1:11,]
-good.timeData
-?apply
+# good.timeData
 good.timeData.diffs.df = apply(good.timeData, 2, diff)
-dim(good.timeData.diffs.df)
+# dim(good.timeData.diffs.df)
 dim(good.timeData.diffs.df)[1] * dim(good.timeData.diffs.df)[2] == length(as.vector(good.timeData.diffs.df))
 # Should be TRUE, and is
 # 
@@ -34,7 +50,7 @@ abline(v = mean(good.timeData.diffs.vt), col = "red")
 dev.off()
 # 
 # Mean and median don't really feel right here, ca. double the number of tags are in the class 913405 - 913410
-table(good.timeData.diffs.vt)
+# table(good.timeData.diffs.vt)
 png(file = "table-plot.png", width = 7, height = 7, units = "in", res = 300)
 plot(table(good.timeData.diffs.vt), las = 2, xlab = "")
 dev.off()
@@ -42,4 +58,105 @@ dev.off()
 # Surprisingly sparse range of values
 # Looks like there are two modal classes essentially, so the median might be a good pick after all.
 med.cycle.diff = median(good.timeData.diffs.vt)
+med.cycle.diff
+# 
+# Ok, so I had the crazy thought that the cycle to cycle difference might depend on plate location
+# Examine this using a long dataframe and ggplot2
+library(reshape2)
+?melt
+good.timeData.diffs.df.long = melt(good.timeData.diffs.df,
+									value.name = "time.ms")
+# head(good.timeData.diffs.df.long)
+#
+# Adding a row variable
+plate.rows = rep(unique(plate.letters), each = 10 * 12) 
+good.timeData.diffs.df.long$plate.rows = plate.rows
+# 
+library(ggplot2)
+# 
+# colour blind friendly qualitative palette from http://jfly.iam.u-tokyo.ac.jp/color/
+cbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+ggplot(good.timeData.diffs.df.long, aes(x = as.factor(Var1), y = time.ms, colour = plate.rows)) +
+	geom_jitter() +
+	xlab("Cycle No.") +
+	scale_colour_manual(values=cbPalette) +
+	geom_hline(aes(yintercept = med.cycle.diff), linetype = "dashed", size = 0.25) +
+	ggtitle("Cycle to cycle time difference; colour by plate row, line at median") +
+	theme(panel.border = element_rect(fill = NA, colour = "black"),
+          axis.title.x = element_text(vjust = 0),
+          axis.title.y = element_text(vjust = 0.2),
+          plot.title = element_text(vjust = 0.8))
+	ggsave("cycle-diffs-by-row.png", dpi = 300)
+# 
+# Now colour by plate column
+plate.columns = rep(rep(1:12, each = 10), 8)
+good.timeData.diffs.df.long$plate.columns = plate.columns
+# head(good.timeData.diffs.df.long)
+# good.timeData.diffs.df.long
+# 
+# had to use the default colour because of the number of classes in factor(plate.columns)
+ggplot(good.timeData.diffs.df.long, aes(x = as.factor(Var1), y = time.ms, colour = factor(plate.columns))) +
+	geom_jitter() +
+	xlab("Cycle No.") +
+	geom_hline(aes(yintercept = med.cycle.diff), linetype = "dashed", size = 0.25) +
+	ggtitle("Cycle to cycle time difference; colour by plate column, line at median") +
+	theme(panel.border = element_rect(fill = NA, colour = "black"),
+          axis.title.x = element_text(vjust = 0),
+          axis.title.y = element_text(vjust = 0.2),
+          plot.title = element_text(vjust = 0.8))
+	ggsave("cycle-diffs-by-column.png", dpi = 300)
+# 
+# Ok, I think I've had enough of this - there's no obvious association by plate column - TODO: could look at the
+# distribution of cycle-to-cycle diffs by column and row, but I'm starting to doubt the utility of this.
+# 
+# Constructing the actual time base:
+# 
+# Actually, before I do, I should look at the time delay between the two halves of the plate, to see how that
+# varies from cycle to cycle. Cycle to cycle analysis suggests it must do.
+# 
+# E12 - A12 should give the right value
+good.timeData$E12 - good.timeData$A12
+good.timeData$F12 - good.timeData$B12
+good.timeData$G12 - good.timeData$C12
+good.timeData$H12 - good.timeData$D12
+# 
+# Yep, it varies; but, at least it's the same between rows
+# 
+png(file = "time-delay-between-plate-halves-byCycle.png", width = 7, height = 7, units = "in", res = 300)
+plot(1:11, good.timeData$E12 - good.timeData$A12, 
+		xlab = "Cycle No.", 
+		ylab = "Time delay between well A12 and E12, ms",
+		type = "b")
+abline(h = median(good.timeData$E12 - good.timeData$A12), col = "red")
+abline(h = mean(good.timeData$E12 - good.timeData$A12), col = "blue")
+dev.off()
+png(file = "time-delay-between-plate-halves-freq.png", width = 7, height = 7, units = "in", res = 300)
+plot(table(good.timeData$E12 - good.timeData$A12), 
+	las = 2,
+	xlab = "Time delay between well A12 and E12, ms",
+	ylab = "Frequency")
+dev.off()
+# 
+# I choose the median
+med.timeDelay.plate.halves = median(good.timeData$E12 - good.timeData$A12)
+# 
+# 
+# 
+tecanTime = data.frame(row = rep(plate.letters, no.cycles),
+					   column = rep(plate.numbers, no.cycles),
+					   well = rep(wells, no.cycles),
+					   cycle = rep(1:no.cycles, each = no.cycles))
+tecanTime
+
+expt.time = numeric()
+
+for (i in 1:nrow(tecanTime)) {
+	if(tecanTime$row[i] %in% LETTERS[1:4]) {
+		expt.time[i] = ((tecanTime$column[i] -1) * 640) + ((tecanTime$cycle[i] -1) * med.cycle.diff)
+	} else {
+		expt.time[i] = median.timeDelay.plate.halves
+	}
+}
+
+head(expt.time, 500)
 
