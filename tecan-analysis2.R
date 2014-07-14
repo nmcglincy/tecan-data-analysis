4507b493c484ddffa9471865cabe85ba185a5ef0
Nicholas McGlincy <nmcglincy@gmail.com>
2014-07-14 09:42:08 -0700

    still working on function

--- tecan-analysis2.R
+++ tecan-analysis2.R
@@ -227,7 +227,38 @@ AUC = grofit::low.integrate(spl.fit$x, spl.fit$y)
 
 # 
 # A function to lapply across the list of results
-
-growth.curve.analysis = function(x) {
-  
+rm(foo)
+rm(spl.fit, firstDeriv, mu, mu.time, secondDeriv, lambda, lambda.time, A, AUC)
+ls()
+growth.curve.analysis = function(foo) {
+# calculating interesting measurements
+  require(grofit)
+  spl.fit = smooth.spline(foo$expt.time/3600000, foo$corrected.abs)
+  firstDeriv = predict(spl.fit, sort(foo$expt.time/3600000), deriv = 1)
+  mu = firstDeriv$y[which.max(firstDeriv$y)]
+  mu.time = firstDeriv$x[which.max(firstDeriv$y)]
+  secondDeriv = predict(spl.fit, sort(foo$expt.time/3600000), deriv = 2)
+  lambda = secondDeriv$y[which.max(secondDeriv$y)]
+  lambda.time = secondDeriv$x[which.max(secondDeriv$y)]
+  A = max(spl.fit$y)
+  AUC = grofit::low.integrate(spl.fit$x, spl.fit$y)
+# Making a nice graph
+  png(file = paste(foo$sample, foo$well, ".png" sep = "-"), width = 7, height = 7, units = "in", res = 300)
+  plot(foo$expt.time/3600000, foo$corrected.abs,
+       xlab = "Time, hrs", ylab = "Absorbance, A.U.",
+       main = paste(foo$sample, foo$well, sep = "."))
+  lines(spl.fit, col = "red")
+  lines(firstDeriv$x, firstDeriv$y/max(firstDeriv$y), col = "blue")
+  points(firstDeriv$x, firstDeriv$y/max(firstDeriv$y), col = "blue")
+  abline(v = mu.time, col = "darkgreen", lty = 2)
+  lines(firstDeriv$x, secondDeriv$y/max(secondDeriv$y), col = "purple")
+  points(firstDeriv$x, secondDeriv$y/max(secondDeriv$y), col = "purple")
+  abline(v = lambda.time, col = "darkgreen", lty = 2)
+  abline(h = A, col = "darkgreen", lty = 2)
+  dev.off()
+# A list to hold the results
+  list(spl.fit, firstDeriv, mu, mu.time, secondDeriv, lambda, lambda.time, A, AUC)
 }
+
+gc.analysis = grow
+
