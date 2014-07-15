76823a5058201f0d3a0132ff48743b41660127c5
Nicholas McGlincy <nmcglincy@gmail.com>
2014-07-15 13:41:45 -0700

    EDA of derived measurements

--- tecan-analysis2.R
+++ tecan-analysis2.R
@@ -283,6 +283,73 @@ gc.analysis2 = lapply(tecanData.l, growth.curve.analysis)
 str(gc.analysis2)
 tom = lapply(gc.analysis2, as.data.frame)
 str(tom)
-ldply(tom)
+dick = ldply(tom)
+dick
 sample.info
-sample.info$
+sample.info$sample.well = paste(sample.info$sample, sample.info$well, sep = ".")
+harry = merge(sample.info, dick, by.x = "sample.well", by.y = ".id")
+harry
+# 
+# would be better in long format
+?melt
+length(names(harry))
+monkey = melt(harry, 
+              id.vars = names(harry)[1:7],
+              measure.vars = names(harry)[8:length(names(harry))],
+              value.name = "measurement")
+head(monkey)
+banana = subset(monkey, strain != "NONE")
+banana
+# 
+# Looks good
+# Some EDA
+# TODO - would look better with more sensible order of factors
+banana$sample = factor(banana$sample, levels = rev(unique(banana$sample)))
+
+library(RColorBrewer)
+rev(brewer.pal(8, "Paired"))
+display.brewer.all(n=NULL, type="all", select=NULL, exact.n=TRUE)
+
+ggplot(banana, aes(x = sample, y = measurement, fill = sample)) +
+  geom_point(position = position_jitter(w = 0.25), size = 3, pch = 21, colur = "black") +
+  facet_wrap(~ variable, scales = "free_y") +
+  scale_fill_manual(values = rev(brewer.pal(8, "Paired"))) +
+  theme(panel.border = element_rect(fill = NA, colour = "black"),
+        axis.title.x = element_text(vjust = 0, size = 14),
+        axis.title.y = element_text(vjust = 0.6, size = 14),
+        axis.text.x = element_text(size=11, angle = 90, hjust = 1, vjust = 0.5),
+        axis.text.y  = element_text(size=11),
+        plot.title = element_text(size = 15),
+        legend.text = element_text(size = 11),
+        legend.title = element_text(size = 13),
+        strip.text.x = element_text(size = 13),
+        strip.text.y = element_text(size = 13))
+ggsave("measures.png", dpi = 300)
+# 
+# All looks good, but evidently the values of lambda are noisy, and don't really reflect what I see
+# Adding summary mean and se
+head(banana)
+banana.summ = ddply(banana,
+                    .(sample, strain, media, variable),
+                    summarize,
+                    mean = mean(measurement),
+                    se = sd(measurement)/(sqrt(length(measurement))))
+banana.summ
+ggplot(banana, aes(x = sample, y = measurement)) +
+  geom_crossbar(data = banana.summ, aes(x = sample, y = mean, ymin = mean-se, ymax = mean+se), size = 0.25) +
+  geom_point(position = position_jitter(w = 0.25), size = 3, pch = 21, colur = "black", aes(fill = sample)) +
+  facet_wrap(~ variable, scales = "free_y") +
+  scale_fill_manual(values = rev(brewer.pal(8, "Paired"))) +
+  theme(panel.border = element_rect(fill = NA, colour = "black"),
+        axis.title.x = element_text(vjust = 0, size = 14),
+        axis.title.y = element_text(vjust = 0.6, size = 14),
+        axis.text.x = element_text(size=11, angle = 90, hjust = 1, vjust = 0.5),
+        axis.text.y  = element_text(size=11),
+        plot.title = element_text(size = 15),
+        legend.text = element_text(size = 11),
+        legend.title = element_text(size = 13),
+        strip.text.x = element_text(size = 13),
+        strip.text.y = element_text(size = 13))
+ggsave("measures2.png", dpi = 300)
+# 
+# Would be cooler if media was shape - through all graphs
