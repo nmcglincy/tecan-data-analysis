f2b6b2c74919e9a2624c29cdb056e8269e775e28
Nicholas McGlincy <nmcglincy@gmail.com>
2014-07-15 18:28:53 -0700

    a good tidy up

--- growth-curve-analysis-Alg2.R
+++ growth-curve-analysis-Alg2.R
@@ -1,20 +1,30 @@
 growth.curve.analysis = function(foo) {
-   # calculating interesting measurements
-  library(grofit)
+# calculating interesting characteristics of the growth curve, fitting a smooth.spline()
+# on an hour time-base.
+  require(grofit)
   spl.fit = smooth.spline(foo$expt.time/3600000, foo$corrected.abs)
   firstDeriv = predict(spl.fit, sort(foo$expt.time/3600000), deriv = 1)
   mu = firstDeriv$y[which.max(firstDeriv$y)]
   mu.time = firstDeriv$x[which.max(firstDeriv$y)]
-#   secondDeriv = predict(spl.fit, sort(foo$expt.time/3600000), deriv = 2)
-#   lambda = secondDeriv$y[which.max(secondDeriv$y)]
-#   lambda.time = secondDeriv$x[which.max(secondDeriv$y)]
+#
+# These functions are left over from my first attempt: using the maximum of 
+# the second derivative of the spline fit to find the point of maximum accelration.
+# This proved too sensitive to noise in the lag-phase measurements. The values of
+# lambda were highly variable within groups, and didn't seem to reflect my visual
+# impression of the curves.
+	# secondDeriv = predict(spl.fit, sort(foo$expt.time/3600000), deriv = 2)
+	# lambda = secondDeriv$y[which.max(secondDeriv$y)]
+	# lambda.time = secondDeriv$x[which.max(secondDeriv$y)]
 # 
-# new lambda is eqv to lambda.time
+# new lambda is eqv to lambda.time, defined here as the x-axis intercept of the 
+# straight line from the log-phase. This seems to be a bit conservative (there is 
+# a clear inflection of the curve lambda - an "acceleration phase"), but is more 
+# stable and better reflects my visual impression of the curves.
   lambda = -(spl.fit$y[which.max(firstDeriv$y)] - (mu * mu.time))/mu
   A = max(spl.fit$y)
   AUC = grofit::low.integrate(spl.fit$x, spl.fit$y)
-  # Making a nice graph
-  # TODO - a legend would be nice
+# Making a nice graph
+# TODO - a legend would be nice
   png(file = paste(paste(foo$sample, foo$well, sep = "-"), ".png", sep = ""), 
       width = 7, height = 7, units = "in", res = 300)
   plot(foo$expt.time/3600000, foo$corrected.abs, ylim = c(0,1),
@@ -23,13 +33,17 @@ growth.curve.analysis = function(foo) {
   lines(firstDeriv$x, firstDeriv$y/max(firstDeriv$y), col = "blue")
   points(firstDeriv$x, firstDeriv$y/max(firstDeriv$y), col = "blue")
   abline(v = mu.time, col = "darkgreen", lty = 2)
+# 
+# Left over from the first method:
 #   lines(firstDeriv$x, secondDeriv$y/max(secondDeriv$y), col = "purple")
 #   points(firstDeriv$x, secondDeriv$y/max(secondDeriv$y), col = "purple")
 #   abline(v = lambda.time, col = "darkgreen", lty = 2)
   abline(h = A, col = "darkgreen", lty = 2)
   abline(a = spl.fit$y[which.max(firstDeriv$y)] - (mu * mu.time) , b = mu)
-  abline(v = lambda, col = "orange")
+  abline(v = lambda, col = "orange", lty = 2)
   dev.off()
-  # A list to hold the results, TODO - needs some names, and a more user friendly order
+# 
+# A list to hold the results
+# TODO - include the fit and derivative objects maybe?
   list(mu =  mu, lambda = lambda, A = A, AUC = AUC)
 } 
--- tecan-analysis2.R
+++ tecan-analysis2.R
@@ -51,7 +51,9 @@ library(ggplot2)
 #           strip.text.x = element_text(size = 12),
 #           strip.text.y = element_text(size = 12))
 # ggsave("rawLines-facet-media.png", dpi = 400)
+
 tecanData.negative = subset(tecanData, strain == "NONE" )
+
 # ggplot(tecanData.negative, aes(x = expt.time/3600000, y = abs, colour = sample)) +
 #     geom_point() +
 #     geom_line(aes(group = well)) +
@@ -67,14 +69,13 @@ tecanData.negative = subset(tecanData, strain == "NONE" )
 #           legend.title = element_text(size = 14))
 # ggsave("raw-neg-wells.png", dpi = 400)
 # 
+# Background correction
 library(plyr)
 tecanData.neg.summ = ddply(tecanData.negative,
                           .(well),
                           summarize,
                           well.median = median(abs))
 sample.info = data[[2]]
-# ls()
-# sample.info
 tecanData.neg.summ = merge(tecanData.neg.summ,
                            sample.info,
                            by.x = "well",
@@ -83,15 +84,14 @@ media.bkgd = ddply(tecanData.neg.summ,
                     .(media),
                     summarize,
                     media.median = median(well.median))
-# media.bkgd
 tecanData = tecanData[order(tecanData$media),]
-# tecanData
 bkgd = c(rep(media.bkgd$media.median[1], length(which(tecanData$media == "AA-SHOCK"))), 
          rep(media.bkgd$media.median[2], length(which(tecanData$media == "YEPD"))))
 tecanData = data.frame(tecanData, bkgd)
 tecanData$corrected.abs = tecanData$abs - tecanData$bkgd
-
-# head(tecanData)
+# 
+# EDA plots of the background corrected data - didn't save these ones, just for visual confirmation
+# that the general trends survived the background correction.
 # ggplot(tecanData, aes(x = expt.time/3600000, y = corrected.abs, colour = sample, group = well)) +
 #     geom_point() +
 #     scale_colour_brewer(palette = "Paired") +
@@ -138,179 +138,23 @@ tecanData$corrected.abs = tecanData$abs - tecanData$bkgd
 #           legend.title = element_text(size = 14),
 #           strip.text.x = element_text(size = 12),
 #           strip.text.y = element_text(size = 12))
-
+# 
 # Growth curve analysis
-library(grofit)
-head(tecanData)
-# 
-# writing my own grofit smooth.spline function
-# 
-# pseudo-code:
-# will work quicker on a list by sample
-# spline fit
-tecanData.l = dlply(tecanData, .(sample, well))
-# str(tecanData.l)
-# names(tecanData.l)
-foo = tecanData.l[[43]]
-# foo
-# with(foo, plot(expt.time/3600000, corrected.abs))
-
-spl.fit = smooth.spline(foo$expt.time/3600000, foo$corrected.abs)
-# plot(foo$expt.time/3600000, foo$corrected.abs)
-# lines(spl.fit, col = "red")
-
-firstDeriv = predict(spl.fit, sort(foo$expt.time/3600000), deriv = 1)
-# firstDeriv
-# plot(foo$expt.time/3600000, foo$corrected.abs)
-# lines(spl.fit, col = "red")
-# lines(firstDeriv$x, firstDeriv$y/max(firstDeriv$y), col = "blue")
-
-# calculating mu
-# Change in abs per hour, because fit was on time/3600000
-mu = firstDeriv$y[which.max(firstDeriv$y)]
-mu
-mu.time = firstDeriv$x[which.max(firstDeriv$y)]
-mu.time 
-
-# deriving line based on mu
-# at mu.time, the equivalent value of spl.fit is:
-spl.fit$y[which.max(firstDeriv$y)]
-# [1] 0.4456126
-
-
-# plot(foo$expt.time/3600000, foo$corrected.abs)
-# lines(spl.fit, col = "red")
-# lines(firstDeriv$x, firstDeriv$y/max(firstDeriv$y), col = "blue")
-# points(firstDeriv$x, firstDeriv$y/max(firstDeriv$y), col = "blue")
-# abline(v = mu.time, col = "green", lty = 2)
-
-#
-# calculating lambda - the time of the lag phase
-secondDeriv = predict(spl.fit, sort(foo$expt.time/3600000), deriv = 2)
-# secondDeriv
-# plot(foo$expt.time/3600000, foo$corrected.abs)
-# lines(spl.fit, col = "red")
-# lines(firstDeriv$x, firstDeriv$y/max(firstDeriv$y), col = "blue")
-# points(firstDeriv$x, firstDeriv$y/max(firstDeriv$y), col = "blue")
-# abline(v = mu.time, col = "green", lty = 2)
-# lines(firstDeriv$x, secondDeriv$y/max(secondDeriv$y), col = "purple")
-# points(firstDeriv$x, secondDeriv$y/max(secondDeriv$y), col = "purple")
-
-lambda = secondDeriv$y[which.max(secondDeriv$y)]
-# lambda
-lambda.time = secondDeriv$x[which.max(secondDeriv$y)]
-# lambda.time
-
-# plot(foo$expt.time/3600000, foo$corrected.abs)
-# lines(spl.fit, col = "red")
-# lines(firstDeriv$x, firstDeriv$y/max(firstDeriv$y), col = "blue")
-# points(firstDeriv$x, firstDeriv$y/max(firstDeriv$y), col = "blue")
-# abline(v = mu.time, col = "green", lty = 2)
-# lines(firstDeriv$x, secondDeriv$y/max(secondDeriv$y), col = "purple")
-# points(firstDeriv$x, secondDeriv$y/max(secondDeriv$y), col = "purple")
-# abline(v = lambda.time, col = "green", lty = 2)
-
-# 
-# the maximum growth level, A
-# str(spl.fit)
-A = max(spl.fit$y)
-# A
-
-plot(foo$expt.time/3600000, foo$corrected.abs)
-lines(spl.fit, col = "red")
-lines(firstDeriv$x, firstDeriv$y/max(firstDeriv$y), col = "blue")
-points(firstDeriv$x, firstDeriv$y/max(firstDeriv$y), col = "blue")
-abline(v = mu.time, col = "darkgreen", lty = 2)
-lines(firstDeriv$x, secondDeriv$y/max(secondDeriv$y), col = "purple")
-points(firstDeriv$x, secondDeriv$y/max(secondDeriv$y), col = "purple")
-abline(v = lambda.time, col = "darkgreen", lty = 2)
-abline(h = A, col = "darkgreen", lty = 2)
-abline(-0.745, 0.134)
-
-mu
-mu.time
-mu * mu.time
-spl.fit$y[which.max(firstDeriv$y)]
-abline(a = spl.fit$y[which.max(firstDeriv$y)] - (mu * mu.time) , b = mu)
-rm(foo)
-# TODO - consider the utility of this approach on another growth model using grofit
-
-# 
-# the AUC - 
-AUC = grofit::low.integrate(spl.fit$x, spl.fit$y)
-# AUC
-
-# 
-# A function to lapply across the list of results
-rm(foo)
-rm(spl.fit, firstDeriv, mu, mu.time, secondDeriv, lambda, lambda.time, A, AUC)
-ls()
-growth.curve.analysis = function(foo) {
-# calculating interesting measurements
-  require(grofit)
-  spl.fit = smooth.spline(foo$expt.time/3600000, foo$corrected.abs)
-  firstDeriv = predict(spl.fit, sort(foo$expt.time/3600000), deriv = 1)
-  mu = firstDeriv$y[which.max(firstDeriv$y)]
-  mu.time = firstDeriv$x[which.max(firstDeriv$y)]
-  secondDeriv = predict(spl.fit, sort(foo$expt.time/3600000), deriv = 2)
-  lambda = secondDeriv$y[which.max(secondDeriv$y)]
-  lambda.time = secondDeriv$x[which.max(secondDeriv$y)]
-  A = max(spl.fit$y)
-  AUC = grofit::low.integrate(spl.fit$x, spl.fit$y)
-# Making a nice graph
-# TODO - a legend would be nice
-  png(file = paste(paste(foo$sample, foo$well, sep = "-"), ".png", sep = ""), width = 7, height = 7, units = "in", res = 300)
-  plot(foo$expt.time/3600000, foo$corrected.abs, ylim = c(0,1),
-       xlab = "Time, hrs", ylab = "Absorbance, A.U.")
-  lines(spl.fit, col = "red")
-  lines(firstDeriv$x, firstDeriv$y/max(firstDeriv$y), col = "blue")
-  points(firstDeriv$x, firstDeriv$y/max(firstDeriv$y), col = "blue")
-  abline(v = mu.time, col = "darkgreen", lty = 2)
-  lines(firstDeriv$x, secondDeriv$y/max(secondDeriv$y), col = "purple")
-  points(firstDeriv$x, secondDeriv$y/max(secondDeriv$y), col = "purple")
-  abline(v = lambda.time, col = "darkgreen", lty = 2)
-  abline(h = A, col = "darkgreen", lty = 2)
-  abline(a = spl.fit$y[which.max(firstDeriv$y)] - (mu * mu.time) , b = mu)
-  dev.off()
-# A list to hold the results, TODO - needs some names, and a more user friendly order
-  list(mu =  mu, lambda = lambda.time, A = A, AUC = AUC)
-}
-
-gc.analysis = growth.curve.analysis(tecanData.l[[43]])
-gc.analysis
-
-gc.analysis2 = lapply(tecanData.l, growth.curve.analysis)
-str(gc.analysis2)
-tom = lapply(gc.analysis2, as.data.frame)
-str(tom)
-dick = ldply(tom)
-dick
-sample.info
+source("growth-curve-analysis-Alg2.R")
+gc.analysis = lapply(tecanData.l, growth.curve.analysis)
+gc.analysis.df = ldply(lapply(gc.analysis2, as.data.frame))
 sample.info$sample.well = paste(sample.info$sample, sample.info$well, sep = ".")
-harry = merge(sample.info, dick, by.x = "sample.well", by.y = ".id")
-harry
+gc.analysis.df = merge(sample.info, gc.analysis.df, by.x = "sample.well", by.y = ".id")
+gc.analysis.dfl = melt(gc.analysis.df, 
+              				id.vars = names(gc.analysis.df)[1:7],
+              				measure.vars = names(gc.analysis.df)[8:length(names(gc.analysis.df))],
+              				value.name = "measurement")
+gc.analysis.dfl = subset(gc.analysis.dfl, strain != "NONE")
 # 
-# would be better in long format
-?melt
-length(names(harry))
-monkey = melt(harry, 
-              id.vars = names(harry)[1:7],
-              measure.vars = names(harry)[8:length(names(harry))],
-              value.name = "measurement")
-head(monkey)
-banana = subset(monkey, strain != "NONE")
-banana
-# 
-# Looks good
 # Some EDA
-# TODO - would look better with more sensible order of factors
-banana$sample = factor(banana$sample, levels = rev(unique(banana$sample)))
-
-library(RColorBrewer)
-rev(brewer.pal(8, "Paired"))
-display.brewer.all(n=NULL, type="all", select=NULL, exact.n=TRUE)
-
-ggplot(banana, aes(x = sample, y = measurement, fill = sample)) +
+# reorder factors - might be better to do this earlier
+gc.analysis.dfl$sample = factor(gc.analysis.dfl$sample, levels = rev(unique(gc.analysis.dfl$sample)))
+ggplot(gc.analysis.dfl, aes(x = sample, y = measurement, fill = sample)) +
   geom_point(position = position_jitter(w = 0.25), size = 3, pch = 21, colur = "black") +
   facet_wrap(~ variable, scales = "free_y") +
   scale_fill_manual(values = rev(brewer.pal(8, "Paired"))) +
@@ -324,19 +168,16 @@ ggplot(banana, aes(x = sample, y = measurement, fill = sample)) +
         legend.title = element_text(size = 13),
         strip.text.x = element_text(size = 13),
         strip.text.y = element_text(size = 13))
-ggsave("measures.png", dpi = 300)
+# ggsave("measures.png", dpi = 300)
 # 
-# All looks good, but evidently the values of lambda are noisy, and don't really reflect what I see
 # Adding summary mean and se
-head(banana)
-banana.summ = ddply(banana,
-                    .(sample, strain, media, variable),
-                    summarize,
-                    mean = mean(measurement),
-                    se = sd(measurement)/(sqrt(length(measurement))))
-banana.summ
-ggplot(banana, aes(x = sample, y = measurement)) +
-  geom_crossbar(data = banana.summ, aes(x = sample, y = mean, ymin = mean-se, ymax = mean+se), size = 0.25) +
+gc.analysis.dfl.summ = ddply(gc.analysis.dfl,
+    				                .(sample, strain, media, variable),
+            				        summarize,
+                    				mean = mean(measurement),
+                    				se = sd(measurement)/(sqrt(length(measurement))))
+ggplot(gc.analysis.dfl, aes(x = sample, y = measurement)) +
+  geom_crossbar(data = gc.analysis.dfl.summ, aes(x = sample, y = mean, ymin = mean-se, ymax = mean+se), size = 0.25) +
   geom_point(position = position_jitter(w = 0.25), size = 3, pch = 21, colur = "black", aes(fill = sample)) +
   facet_wrap(~ variable, scales = "free_y") +
   scale_fill_manual(values = rev(brewer.pal(8, "Paired"))) +
@@ -350,6 +191,7 @@ ggplot(banana, aes(x = sample, y = measurement)) +
         legend.title = element_text(size = 13),
         strip.text.x = element_text(size = 13),
         strip.text.y = element_text(size = 13))
-ggsave("measures2.png", dpi = 300)
+# ggsave("measures2-alg2.png", dpi = 300)
 # 
 # Would be cooler if media was shape - through all graphs
+# Summary graph with the smooth.spline on all data per sample would be cool too
