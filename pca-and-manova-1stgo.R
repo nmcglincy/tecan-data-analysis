6a1d4378a7dd4d7f333f0f3b866a091df49c62d3
Nicholas McGlincy <nmcglincy@gmail.com>
2014-07-22 12:08:17 -0700

    Working on anova of PC1

--- pca-and-manova-1stgo.R
+++ pca-and-manova-1stgo.R
@@ -4,20 +4,18 @@ gc.analysis.df.lite = gc.analysis.df[,5:length(gc.analysis.df)]
 # row.names(gc.analysis.df.lite) = gc.analysis.df.lite$sample
 
 gc.pca = prcomp(~mu + lambda + A + AUC, data = gc.analysis.df.lite, scale = TRUE, center = TRUE)
-capture.output(gc.pca
-summ = summary(gc.pca)$importance
-summ = as.data.frame(summ)
-prop.var = summ[2,]
+gc.pca
+
+# capture.output(gc.pca)
+# summ = summary(gc.pca)$importance
+# summ = as.data.frame(summ)
+
+prop.var = as.data.frame(summary(gc.pca)$importance)[2,]
 varExplained = data.frame(PC = 1:4, prop.variance = t(prop.var)[,1])
 varExplained
-plot(t(prop.var), type = "b")
 
-
-str(gc.pca)
-biplot(gc.pca)
-class(gc.pca$x)
 gc.pc = data.frame(gc.analysis.df.lite[,1:5], as.data.frame(gc.pca$x))
-
+gc.pc
 
 ggplot(gc.pc, aes(x = PC1, y = PC2, fill = sample)) +
   geom_point(size = 3.5, colour = "black", pch = 21) +
@@ -31,7 +29,7 @@ ggplot(gc.pc, aes(x = PC1, y = PC2, fill = sample)) +
         plot.title = element_text(size = 16),
         legend.text = element_text(size = 12),
         legend.title = element_text(size = 14))
-ggsave("pca-biplot.png", dpi = 300)
+# ggsave("pca-biplot.png", dpi = 300)
 
 ggplot(varExplained, aes(x = factor(PC), y = prop.variance)) +
   geom_bar(stat = "identity") +
@@ -45,9 +43,45 @@ ggplot(varExplained, aes(x = factor(PC), y = prop.variance)) +
         plot.title = element_text(size = 16),
         legend.text = element_text(size = 12),
         legend.title = element_text(size = 14))
-ggsave("pca-varPlot.png", dpi = 300)
+# ggsave("pca-varPlot.png", dpi = 300)
 
 # PC2 looks kind of meaningful, even is the proportion of variance explained is very low
+# anova of PC1
+pc1.aov = aov(PC1 ~ media * gcn20.genotype * arb1.genotype, data = gc.pc)
+pc1.aov
+summary(pc1.aov)
+capture.output(summary(pc1.aov), file = "summary-pc1-anova.txt")
+
+
+cbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+ggplot(gc.pc, aes(x = sample, y = PC1, colour = media, shape = strain)) +
+  geom_point(position = position_jitter(w = 0.15), size = 2.5) +
+  scale_colour_manual(values=cbPalette[2:3]) +
+  theme(panel.border = element_rect(fill = NA, colour = "black"),
+        axis.title.x = element_text(vjust = 0, size = 14),
+        axis.title.y = element_text(vjust = 0.4, size = 14),
+        axis.text.x = element_text(size = 10, angle = 90, hjust = 1, vjust = 0.5),
+        axis.text.y  = element_text(size = 10),
+        plot.title = element_text(size = 14),
+        legend.text = element_text(size = 10),
+        legend.title = element_text(size = 12),
+        strip.text.x = element_text(size = 12),
+        strip.text.y = element_text(size = 12))
+
+foo = TukeyHSD(pc1.aov, ordered = TRUE)
+foo
+# 
+# I think I'm overfitting.... too many DF = 1 and p adj = 0
+
+pc1.aov2 = aov(PC1 ~ media * strain, data = gc.pc)
+pc1.aov2
+summary(pc1.aov2)
+capture.output(summary(pc1.aov2), file = "summary-pc1-anova2.txt")
+bar = TukeyHSD(pc1.aov2)
+capture.output(TukeyHSD(pc1.aov2), file = "summary-HSD-pc1-anova2.txt")
+bar
+
+# This seems fairer somehow...
 
 # 
 ?manova
