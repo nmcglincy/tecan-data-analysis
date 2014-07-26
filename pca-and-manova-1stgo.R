2f41074f19e87827297acfa09b89ce278bfeeee8
Nicholas McGlincy <nmcglincy@gmail.com>
2014-07-26 12:00:40 -0700

    refining stat analysis

--- pca-and-manova-1stgo.R
+++ pca-and-manova-1stgo.R
@@ -89,6 +89,7 @@ ggsave("pc1-plot.png", dpi = 300)
 
 foo = TukeyHSD(pc1.aov, ordered = TRUE)
 foo
+# Need a report of this I think...
 # 
 # I think I'm overfitting.... too many DF = 1 and p adj = 0
 
@@ -99,38 +100,45 @@ capture.output(summary(pc1.aov2), file = "summary-pc1-anova2.txt")
 bar = TukeyHSD(pc1.aov2)
 capture.output(TukeyHSD(pc1.aov2), file = "summary-HSD-pc1-anova2.txt")
 bar
-
+# 
 # This seems fairer somehow...
 
-# 
-?manova
-## Example on producing plastic film from Krzanowski (1998, p. 381)
-tear <- c(6.5, 6.2, 5.8, 6.5, 6.5, 6.9, 7.2, 6.9, 6.1, 6.3,
-          6.7, 6.6, 7.2, 7.1, 6.8, 7.1, 7.0, 7.2, 7.5, 7.6)
-gloss <- c(9.5, 9.9, 9.6, 9.6, 9.2, 9.1, 10.0, 9.9, 9.5, 9.4,
-           9.1, 9.3, 8.3, 8.4, 8.5, 9.2, 8.8, 9.7, 10.1, 9.2)
-opacity <- c(4.4, 6.4, 3.0, 4.1, 0.8, 5.7, 2.0, 3.9, 1.9, 5.7,
-             2.8, 4.1, 3.8, 1.6, 3.4, 8.4, 5.2, 6.9, 2.7, 1.9)
-Y <- cbind(tear, gloss, opacity)
-Y
-rate <- factor(gl(2,10), labels = c("Low", "High"))
-additive <- factor(gl(2, 5, length = 20), labels = c("Low", "High"))
-rate
-additive
-fit <- manova(Y ~ rate * additive)
-summary.aov(fit)             # univariate ANOVA tables
-summary(fit, test = "Wilks") # ANOVA table of Wilks' lambda
-summary(fit)                # same F statistics as single-df terms
+# MANOVA analysis of the actual variables
+#
+# From the Docs...
+# ?manova
+# ## Example on producing plastic film from Krzanowski (1998, p. 381)
+# tear <- c(6.5, 6.2, 5.8, 6.5, 6.5, 6.9, 7.2, 6.9, 6.1, 6.3,
+#           6.7, 6.6, 7.2, 7.1, 6.8, 7.1, 7.0, 7.2, 7.5, 7.6)
+# gloss <- c(9.5, 9.9, 9.6, 9.6, 9.2, 9.1, 10.0, 9.9, 9.5, 9.4,
+#            9.1, 9.3, 8.3, 8.4, 8.5, 9.2, 8.8, 9.7, 10.1, 9.2)
+# opacity <- c(4.4, 6.4, 3.0, 4.1, 0.8, 5.7, 2.0, 3.9, 1.9, 5.7,
+#              2.8, 4.1, 3.8, 1.6, 3.4, 8.4, 5.2, 6.9, 2.7, 1.9)
+# Y <- cbind(tear, gloss, opacity)
+# Y
+# rate <- factor(gl(2,10), labels = c("Low", "High"))
+# additive <- factor(gl(2, 5, length = 20), labels = c("Low", "High"))
+# rate
+# additive
+# fit <- manova(Y ~ rate * additive)
+# summary.aov(fit)             # univariate ANOVA tables
+# summary(fit, test = "Wilks") # ANOVA table of Wilks' lambda
+# summary(fit)                # same F statistics as single-df terms
 
+ls()
 
 gc.analysis.df.lite
-fit = manova(Y ~ media * gcn20.genotype * arb1.genotype ,data = gc.analysis.df.lite)
 Y = as.matrix(gc.analysis.df.lite[,6:9])
 Y
+manova.fit = manova(Y ~ media * strain ,data = gc.analysis.df.lite)
+summary(manova.fit, test = "Wilks")
+summary.aov(manova.fit)
 
-summary.aov(fit)
-summary(fit, test = "Wilks")
+manova.fit2 = manova(Y ~ media * arb1.genotype * gcn20.genotype ,data = gc.analysis.df.lite)
+summary(manova.fit2, test = "Wilks")
+summary.aov(manova.fit2)
 
 
-fit2 = manova(Y ~ gcn20.genotype * media * arb1.genotype ,data = gc.analysis.df.lite)
-summary(fit2, test = "Wilks")
+
+
+
