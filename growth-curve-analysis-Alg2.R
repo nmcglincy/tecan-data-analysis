7165ca6a0948976ab5702464c1660ba2c48e6764
Nicholas McGlincy <nmcglincy@gmail.com>
2014-07-28 16:08:13 -0700

    updating and reformatting function

--- growth-curve-analysis-Alg2.R
+++ growth-curve-analysis-Alg2.R
@@ -29,20 +29,35 @@ growth.curve.analysis = function(foo) {
 # TODO - a legend would be nice
   png(file = paste(paste(foo$sample, foo$well, sep = "-"), ".png", sep = ""), 
       width = 7, height = 7, units = "in", res = 300)
-  plot(foo$expt.time/3600000, foo$corrected.abs, ylim = c(0,1),
-       xlab = "Time, hrs", ylab = "Absorbance, A.U.")
-  lines(spl.fit, col = "red")
-  lines(firstDeriv$x, firstDeriv$y/max(firstDeriv$y), col = "blue")
-  points(firstDeriv$x, firstDeriv$y/max(firstDeriv$y), col = "blue")
-  abline(v = mu.time, col = "darkgreen", lty = 2)
+  plot(foo$expt.time/3600000, 
+       foo$corrected.abs, 
+       ylim = c(0,1),
+       xlab = "Time, hrs", 
+       ylab = "Absorbance, A.U.")
+  lines(spl.fit, 
+        col = "red")
+  lines(firstDeriv$x, 
+        firstDeriv$y/max(firstDeriv$y), 
+        col = "blue")
+  points(firstDeriv$x, 
+         firstDeriv$y/max(firstDeriv$y), 
+         col = "blue")
+  abline(v = mu.time, 
+         col = "darkgreen", 
+         lty = 2)
 # 
 # Left over from the first method:
 #   lines(firstDeriv$x, secondDeriv$y/max(secondDeriv$y), col = "purple")
 #   points(firstDeriv$x, secondDeriv$y/max(secondDeriv$y), col = "purple")
 #   abline(v = lambda.time, col = "darkgreen", lty = 2)
-  abline(h = A, col = "darkgreen", lty = 2)
-  abline(a = spl.fit$y[which.max(firstDeriv$y)] - (mu * mu.time) , b = mu)
-  abline(v = lambda, col = "orange", lty = 2)
+  abline(h = A, 
+         col = "darkgreen", 
+         lty = 2)
+  abline(a = spl.fit$y[which.max(firstDeriv$y)] - (mu * mu.time) , 
+         b = mu)
+  abline(v = lambda, 
+         col = "orange", 
+         lty = 2)
   dev.off()
 # 
 # A list to hold the results
